72b5666fbe7f61fbdf03848f601d849a6f41cc9f
Hiba-Mohamed <hibamoha987@gmail.com>
2024-10-24 14:10:56 -0230

    top rated tests written

--- __tests__/utils/movieUtils.test.js
+++ __tests__/utils/movieUtils.test.js
@@ -1,22 +1,43 @@
-const { getRandomMoviesByGenre, getTopRatedMovies, formatMovieData, getRandomGenre, generateMovieReport } = require("../../utils/movieUtils");
+const {
+  getRandomMoviesByGenre,
+  getTopRatedMovies,
+  formatMovieData,
+  getRandomGenre,
+  generateMovieReport,
+  getRandomNumberOfMovies,
+} = require("../../utils/movieUtils");
 
-describe('Movie Utility Functions', () => {
-    describe('getMoviesByGenre', () => {
+describe("Movie Utility Functions", () => {
+  //     describe('getMoviesByGenre', () => {
 
+  //     });
+
+  describe("getTopRatedMovies", () => {
+
+    test("Returns an Array of the specified length", () => {
+      const array = getTopRatedMovies(15);
+      expect(array.length).toBe(15);
     });
+    test("Organize the array in descending order", () => {
+      const array = getTopRatedMovies(15);
 
-    describe('getTopRatedMovies', () => {
-
+      for (let i = 0; i < array.length - 1; i++) {
+        expect(array[i].rating).toBeGreaterThanOrEqual(array[i + 1].rating);
+      }
     });
+  });
 
-    describe('getMovieDetailsById', () => {
-        
-    });
+  // describe('getMovieDetailsById', () => {
 
-    describe('selectRandomMovieId', () => {
+  // });
 
-    });
+      describe('selectRandomMovieId', () => {
+  //  Select a random movie id
+  // A valid movie is returned
+          test("Selects a random movie", () => {});
+          test("The random movie selected is valid", () => {});
+
+      });
 });
 
-
-getRandomMoviesByGenre
+getRandomMoviesByGenre;
