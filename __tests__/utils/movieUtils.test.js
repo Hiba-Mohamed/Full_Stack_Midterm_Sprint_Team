cdc30265b97b094dd0cf97f79f0c54461b7f97db
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-10-28 14:08:20 -0230

    Updated unit test for getMoviesByGenre

--- __tests__/utils/movieUtils.test.js
+++ __tests__/utils/movieUtils.test.js
@@ -1,21 +1,40 @@
 const {
   getRandomMoviesByGenre,
   getTopRatedMovies,
+  getMoviesByGenre,
   formatMovieData,
   getRandomGenre,
   generateMovieReport,
   getRandomNumberOfMovies,
   selectRandomMovieId,
 } = require("../../utils/movieUtils");
-const data = require('../../utils/data')
+const data = require("../../utils/data");
 const movies = data.Movies;
 const availableMoviesNumber = movies.length;
 
 describe("Movie Utility Functions", () => {
+  describe("getMoviesByGenre", () => {
+    const genre = "Action"; // Example genre for testing
+    const numberOfMovies = 5; // Number of movies to retrieve
 
-  //     describe('getMoviesByGenre', () => {
+    test("Specifying a genre will successfully return x movies from that genre", () => {
+      const moviesByGenre = getMoviesByGenre(genre, numberOfMovies);
+      expect(moviesByGenre.length).toBe(numberOfMovies);
 
-  //     });
+      // Check if all returned movies belong to the specified genre
+      moviesByGenre.forEach((movie) => {
+        expect(movie.genre).toBe(genre);
+      });
+    });
+
+    test("If the genre has no movies, then an appropriate response is returned", () => {
+      const moviesByGenre = getMoviesByGenre(
+        "Nonexistent Genre",
+        numberOfMovies
+      );
+      expect(moviesByGenre).toEqual([]);
+    });
+  });
 
   describe("getTopRatedMovies", () => {
     test("Returns an Array of the specified length", () => {
@@ -25,7 +44,9 @@ describe("Movie Utility Functions", () => {
     test("Organize the array in descending order", () => {
       const array = getTopRatedMovies(15);
       for (let i = 0; i < array.length - 1; i++) {
-        expect(array[i].rating).toBeGreaterThanOrEqual(array[i + 1].rating);
+        expect(array[i].rating).toBeGreaterThanOrEqual(
+          array[i + 1].rating
+        );
       }
     });
   });
@@ -36,17 +57,16 @@ describe("Movie Utility Functions", () => {
 
   describe("selectRandomMovieId", () => {
     const randomMovieId = selectRandomMovieId(availableMoviesNumber);
-    console.log(randomMovieId)
+    console.log(randomMovieId);
     const randomMovie = movies.find((movie) => movie.id === randomMovieId);
-    console.log(randomMovie)
+    console.log(randomMovie);
     test("Selects a random movie id that is included in the avaliable movies", () => {
       expect(randomMovieId).toBeGreaterThanOrEqual(0);
       expect(randomMovieId).toBeLessThan(availableMoviesNumber - 1);
     });
     test("The random movie selected is valid", () => {
-        expect(randomMovie).toBeDefined();
-        expect(randomMovie.id).toBe(randomMovieId)
-
+      expect(randomMovie).toBeDefined();
+      expect(randomMovie.id).toBe(randomMovieId);
     });
   });
 });
